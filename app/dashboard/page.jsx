bbd91d9c292195f62cff10ae0210a73c9797a5bf
SUBHAM-6291 <subhamsingh39621@gmail.com>
2025-08-02 20:05:00 +0530

    dashboard routed fix

---
+++ app/dashboard/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import SideBar from '../_components/Side-Bar/SideBar'
+
+const page = () => {
+  return (
+    <div>
+      <SideBar/>
+    </div>
+  )
+}
+
+export default page
