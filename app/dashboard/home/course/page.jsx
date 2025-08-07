9644005d7cb61bc62f6a1d5360295feed0a7663a
SUBHAM-6291 <subhamsingh39621@gmail.com>
2025-08-07 11:46:49 +0530

    Add missing components

--- app/dashboard/home/course/page.jsx
+++ app/dashboard/home/course/page.jsx
@@ -1,10 +1,11 @@
 import React from "react";
-import Course from "@/app/_components/home/course/Course";
 
+import Course  from '@/app/_components/home/course/course'
 const page = () => {
   return (
     <div>
-      <Course />
+      <Course/>
+
     </div>
   );
 };
